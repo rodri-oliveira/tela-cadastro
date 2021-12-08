e72e169930ed2571a1092079fa0ff74e82c1f3fc
rodri-oliveira <rodrigoleao.ci@gmail.com>
2021-12-08 17:46:22 -0300

    corrigindo a logica do olho magico do input

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-let btnSenha = document.querySelector('.fa-eye')
+let btnSenha = document.querySelector('#verSenha')
+let btnConfirmaSenha = document.querySelector('#verConfirmaSenha')
 
 
 
 btnSenha.addEventListener('click', () => {
     let inputSenha = document.querySelector('#senha')
-    let inputConfirmarSenha = document.querySelector('#confirma-senha')
+    
 
     if (inputSenha.getAttribute('type') == 'password') {
         inputSenha.setAttribute('type', 'text')
     }else {
         inputSenha.setAttribute('type', 'password')
     }
+})
+
+btnConfirmaSenha.addEventListener('click', () => {
+    let inputConfirmarSenha = document.querySelector('#confirma-senha')
 
     if (inputConfirmarSenha.getAttribute('type') == 'password') {
         inputConfirmarSenha.setAttribute('type', 'text')
